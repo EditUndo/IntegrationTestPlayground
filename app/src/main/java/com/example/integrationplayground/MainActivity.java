230a8b2365df212d4b797151e382744601683325
EditUndo <patrick.buah.jr@gmail.com>
2020-03-17 15:27:57 +0000

    Initial commit

---
+++ app/src/main/java/com/example/integrationplayground/MainActivity.java
@@ -0,0 +1,204 @@
+package com.example.integrationplayground;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private ImageView profImage;
+    private TextView txtName,txtID, txtLink;
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+       // AppEventsLogger.activateApp(this);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loginButton = findViewById(R.id.login_button);
+
+        txtID = findViewById(R.id.profile_id);
+        txtLink = findViewById(R.id.profile_link);
+        txtName = findViewById(R.id.profile_name);
+
+        profImage = findViewById(R.id.profile_pic);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.setPermissions(Arrays.asList("email","public_profile","user_link"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+
+            }
+
+            @Override
+            public void onCancel()
+            {
+
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Launch Facebook", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void open(View view)
+    {
+        //https://www.facebook.com/sherrymj12
+        //fb://facewebmodal/f?href="
+        //fb://page/1399666226
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/1399666226"));
+
+        startActivity(browserIntent);
+
+    }
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
+        {
+            if(currentAccessToken == null)
+            {
+                txtName.setText("");
+                txtID.setText("");
+                txtLink.setText("");
+
+                profImage.setImageURI(null);
+                Toast.makeText(MainActivity.this, "User Logged Out!",Toast.LENGTH_LONG).show();
+            }
+            else
+                LoadUserProfile(currentAccessToken);
+        }
+    };
+
+    private void LoadUserProfile(AccessToken fbToken)
+    {
+        GraphRequest request = GraphRequest.newMeRequest(fbToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response)
+            {
+                try
+                {
+                    String firstName = object.getString("first_name");
+                    String lastName = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+                    //String link = object.getString("link");
+                    String image_url = "https://graph.facebook.com/" + id + "/picture?type=normal";
+
+                    txtName.setText(firstName + " " + lastName + " " + email);
+                    txtID.setText(id);
+                    //txtLink.setText(link);
+
+                    RequestOptions rO = new RequestOptions();
+                    rO.dontAnimate();
+
+                    Glide.with(MainActivity.this).load(image_url).into(profImage);
+
+
+                }catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle parameters = new Bundle();
+
+        parameters.putString("fields","first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
